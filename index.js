57bdd6ebe840f1838c8ebc0ceabdbc797ff10049
Jaydson Gomes <jayalemao@gmail.com>
2014-06-05 02:25:40 -0300

    Better code structure. New methods for header and content parse. More tests

--- index.js
+++ index.js
@@ -1,14 +1,73 @@
-var merked = require('marked'),
+var marked = require('marked'),
 	fs = require('fs');
 
 var MarkedMetaData = function (file) {
 	
-	var mdFile = fs.readFileSync(file).toString();
+	var mdFile = fs.readFileSync(file).toString(),
+		tokens = ['---','---'],
+
+		/* Convert metadata header into JSON */
+		toJSON = function (data) {
+			var tmpObj = {},
+				lines = data.trim().split('\n');
+
+			lines.forEach(function(line, i) {
+				tmpObj[line.trim().split(':')[0]] = line.trim().split(':')[1];
+			});
+
+			return tmpObj;
+		},
+
+		/* Extracts the header */
+		getMarkdownHeader = function (data) {
+			var strReg = "^" + tokens[0] + "([\\s|\\S]*?)" + tokens[1],
+				reg = new RegExp(strReg),
+				file = reg.exec(mdFile);
+
+			return file ? file[1] : new Error("Can't get the header");
+		},
+
+		/* Extracts the content */
+		getMarkdownContent = function (data) {
+			var strReg = "^ *\\" + tokens[0] + "[^]*" + tokens[1] + "*",
+				reg = new RegExp(strReg, 'g'),
+				content = data.replace(reg, "");
+
+			return content ? content : new Error("Can't get the content");			
+		};
+
+	/* Define the initial and last markdown header token */
+	this.defineTokens = function (inital, last) {
+		if (inital && last) {
+			tokens[0] = inital;
+			tokens[1] = last;
+		}
+	};
+
+	/* Return the tokens */
+	this.getTokens = function () {
+		return tokens;
+	};
+
+	/* Just return the file */
+	this.getFile = function () {
+		return mdFile;
+	};
 
 	/* Return the markdown metadata */
 	this.metadata = function () {
-		return mdFile;
-	}
+			var file = getMarkdownHeader(mdFile);
+
+			if (file) {
+				JSONData = toJSON(file);
+			}
+			return JSONData;
+	};
+
+	this.markdown = function () {
+		var file = getMarkdownContent(mdFile);
+		return file;
+	};
 }
 
 module.exports = MarkedMetaData;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,49 @@
 var should = require('chai').should(),
+	expect = require('chai').expect,
     MarkedMetaData = require('../index');
 
-describe('#simple markdown', function() {
+describe('#simple markdown stuff', function() {
+	var md = new MarkedMetaData('./test/test.md');
+	var md1 = new MarkedMetaData('./test/test1.md');
+
 	it('get a markdown file;', function() {
-		var md = new MarkedMetaData('./test/test.md');
-		md.metadata().should.equal('<!--\nlayout: post\ntitle: hello world\n-->\n#Hello');
+		md.getFile().should.equal('<!--\nlayout: post\ntitle: hello world\n-->\n#Hello');
+	});
+
+	it('#checking default tokens;', function() {
+		expect(md.getTokens()).to.deep.equal(['---','---']);
+	});
+
+	it('#checking new tokens;', function() {
+		md.defineTokens('<!--', '-->');
+		expect(md.getTokens()).to.deep.equal(['<!--','-->']);
+	});
+
+	it('#checking another tokens;', function() {
+		md.defineTokens('{{{', '}}}');
+		expect(md.getTokens()).to.deep.equal(['{{{','}}}']);
+	});
+
+	it('#Getting markdown metadata;', function () {
+		var obj = { layout: ' post', title: ' hello world' };
+		md.defineTokens('<!--', '-->');
+		expect(md.metadata()).to.deep.equal(obj);
+	});
+
+	it('#Getting markdown metadata 1;', function () {
+		var obj = { layout: ' post', title: ' hello world' };
+		md1.defineTokens('---', '---');
+		expect(md1.metadata()).to.deep.equal(obj);
+	});
+
+	it('#Getting parsed markdown;', function () {
+		md.defineTokens('<!--', '-->');
+		md.markdown().should.equal('\n#Hello');
+	});
+
+	it('#Getting parsed markdown 1;', function () {
+		md1.defineTokens('---', '---');
+		md1.markdown().should.equal('\n#Hello 1');
 	});
 });
 
